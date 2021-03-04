139428e4cf3e5ca39fa035709fab8ff059c88445
ziminyuri <zimin.yuri22@gmail.com>
2021-03-04 20:37:04 +0300

    deploy heroku

--- Verbal_Decision_Analysis/settings.py
+++ Verbal_Decision_Analysis/settings.py
@@ -86,6 +86,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
