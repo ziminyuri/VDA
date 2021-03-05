6a72954d5e589e13f6e4dede248bdcb29c58fabc
ziminyuri <zimin.yuri22@gmail.com>
2021-03-05 09:14:43 +0300

    deploy heroku

--- Verbal_Decision_Analysis/settings.py
+++ Verbal_Decision_Analysis/settings.py
@@ -88,13 +88,10 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
